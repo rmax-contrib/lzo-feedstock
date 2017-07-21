722b812eeda42543a01bd8fdc6c1c990c62726ce
Rolando (Max) Espinoza <me@rmax.io>
2017-07-21 14:09:47 -0400

    WIP.

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,8 +12,8 @@ cd cmake-build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
-      -DCMAKE_INSTALL_FULL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
       -DENABLE_SHARED=ON \
       -DENABLE_STATIC=ON \
